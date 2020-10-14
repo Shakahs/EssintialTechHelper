f24ba775c5a8d31921a082d1523f1d4e9a17c3ca
Shakeel Saleemi <shakahs@gmail.com>
2020-10-13 22:55:16 -0700

    Create a simple AjaxButton component to cut down boilerplate.

--- packages/frontend/client-source/components/Case/CaseList.tsx
+++ packages/frontend/client-source/components/Case/CaseList.tsx
@@ -15,6 +15,7 @@ import {
 import { getAPISession } from "../../features/auth/authSelectors";
 import { resetAuthentication } from "../../features/auth/authSlice";
 import CaseComments from "./CaseComments";
+import AjaxButton from "../utility/AjaxButton";
 
 interface Manage2Props {}
 
@@ -75,33 +76,15 @@ const CaseList: React.FunctionComponent<Manage2Props> = (props) => {
             </button>
          </div>
          <div className={"block"}>
-            <button
-               className={"border p-2 mb-3"}
+            <AjaxButton
+               loading={fetchCaseState.loading}
                onClick={() => {
-                  // fetchCurrentCases.run();
-                  // fetchFutureCases.run();
                   dispatch(debouncedFetchCases());
                }}
-               disabled={fetchCaseState.loading}
+               className={"border p-2 mb-3 bg-green-300"}
             >
-               <svg
-                  className={classnames("inline", {
-                     "animate-spin": fetchCaseState.loading,
-                  })}
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  height={30}
-                  width={30}
-               >
-                  <path
-                     fillRule="evenodd"
-                     d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z"
-                     clipRule="evenodd"
-                  />
-               </svg>
-               Refresh
-            </button>
+               <span>Refresh Cases</span>
+            </AjaxButton>
             {fetchCaseState.error && (
                <span className={"text-red-500"}>
                   There was an error refreshing your subcase list.
---
+++ packages/frontend/client-source/components/utility/AjaxButton.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { debouncedFetchCases } from "../../features/cases/caseThunks";
+import { ReactElement } from "react";
+import { buttonStyle } from "../../constants";
+import Bool from "./Bool";
+import LoadingIcon from "../LoadingIcon";
+
+interface AjaxButtonProps {
+   loading: boolean;
+   onClick: () => void;
+   children: ReactElement;
+   className?: string;
+}
+
+const AjaxButton: React.FunctionComponent<AjaxButtonProps> = (props) => (
+   <button
+      className={props.className ?? buttonStyle}
+      onClick={props.onClick}
+      disabled={props.loading}
+   >
+      <Bool if={props.loading}>
+         <span className={"mr-1"}>
+            <LoadingIcon />
+         </span>
+      </Bool>
+      {props.children}
+   </button>
+);
+
+export default AjaxButton;
