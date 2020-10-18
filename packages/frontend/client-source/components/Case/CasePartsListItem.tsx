f09e28e5585bdf9d826138f25f1888d60ce438e8
Shakeel Saleemi <shakahs@gmail.com>
2020-10-17 18:45:10 -0700

    More updates to part tracking display.

--- packages/frontend/client-source/components/Case/CasePartsListItem.tsx
+++ packages/frontend/client-source/components/Case/CasePartsListItem.tsx
@@ -32,8 +32,8 @@ const CasePartsListItem: React.FunctionComponent<CaseSummaryPartsListItemProps> 
       }
    );
 
-   const showDeliveryETA =
-      tracking?.status !== "delivered" && tracking?.est_delivery_date;
+   const lastTracking =
+      tracking?.tracking_details && last(tracking.tracking_details);
 
    const lastUpdated = !!tracking?.updated_at
       ? format(parseJSON(tracking.updated_at), "L/d h:mm b")
@@ -41,47 +41,49 @@ const CasePartsListItem: React.FunctionComponent<CaseSummaryPartsListItemProps> 
 
    return (
       <div>
-         {/*<span className={"mr-1"}>*/}
-         {/*   Status:*/}
-         {/*   <Bool if={trackingFetchState.isLoading}>*/}
-         {/*      <LoadingIcon />*/}
-         {/*   </Bool>*/}
-         {/*   {tracking?.status ?? "Unknown, try again later"}*/}
-         {/*</span>*/}
-
-         {/*{showDeliveryETA && (*/}
-         {/*   <span>*/}
-         {/*      Estimated Delivery:*/}
-         {/*      {format(parseJSON(tracking.est_delivery_date), "L/d h:mm b")}*/}
-         {/*      Last Updated:*/}
-         {/*      {lastUpdated}*/}
-         {/*   </span>*/}
-         {/*)}*/}
+         <span>
+            Tracking:
+            <a
+               className={"text-blue-500 underline ml-1"}
+               href={`https://www.fedex.com/apps/fedextrack/?tracknum=${props.trackingNumber}`}
+            >
+               {props.trackingNumber}
+            </a>
+         </span>
+         <br />
 
          <Bool if={trackingFetchState.isLoading}>
-            <LoadingIcon /> Fetching part status...
+            <LoadingIcon /> Fetching tracking status...
+         </Bool>
+
+         <Bool if={trackingFetchState.isRejected}>
+            <span>{tracking?.status ?? "Status unknown, try again later"}</span>
          </Bool>
 
          <Bool
             if={
-               tracking?.status !== "delivered" && !trackingFetchState.isLoading
+               trackingFetchState.isFulfilled &&
+               tracking?.status !== "delivered"
             }
          >
-            <span className={"mr-1"}>
-               {tracking?.status ?? "Status unknown, try again later"}
-            </span>
             <span>
                Estimated delivery:
                {tracking?.est_delivery_date &&
                   format(parseJSON(tracking.est_delivery_date), "L/d h:mm b")}
             </span>
-            <Bool if={!!tracking?.status}>
-               <br />
-               <span>
-                  Tracking Updated:
-                  {lastUpdated}
-               </span>
-            </Bool>
+            <br />
+            <span>
+               Last location:
+               {lastTracking?.tracking_location?.city}{" "}
+               {lastTracking?.tracking_location?.state}
+               {lastTracking?.datetime &&
+                  format(parseJSON(lastTracking?.datetime), "L/d h:mm b")}
+            </span>
+            <br />
+            <span>
+               Tracking Updated:
+               {lastUpdated}
+            </span>
          </Bool>
 
          <Bool if={tracking?.status === "delivered"}>
