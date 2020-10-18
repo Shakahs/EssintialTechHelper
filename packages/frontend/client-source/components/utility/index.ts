9f5f9e49f69351e01b82e5ed59c858498f91163d
Shakeel Saleemi <shakahs@gmail.com>
2020-10-18 12:57:12 -0700

    Fix getAPISessionInComponent always updating apiSessionCreation date regardless of if the session was actually refreshed.

--- packages/frontend/client-source/components/utility/index.ts
+++ packages/frontend/client-source/components/utility/index.ts
@@ -3,15 +3,23 @@ import { checkAPISession } from "../../features/auth/authThunks";
 import { unwrapResult } from "@reduxjs/toolkit";
 import { updateAPISession } from "../../features/auth/authSlice";
 import { APISession } from "../../api";
+import { getAPISession } from "../../features/auth/authSelectors";
 
 export const getAPISessionInComponent = async (): Promise<APISession> => {
-   const APISessionWrapped = await store.dispatch(checkAPISession());
-   const APISession = unwrapResult(APISessionWrapped);
-   store.dispatch(
-      updateAPISession({
-         apiSessionData: APISession,
-         apiSessionCreation: new Date().toISOString(),
-      })
-   );
-   return APISession;
+   const oldApiSession = getAPISession(store.getState());
+   const newAPISessionWrapped = await store.dispatch(checkAPISession());
+   const newAPISession = unwrapResult(newAPISessionWrapped);
+
+   //compare the session ID we already had to the one checkAPISession returns. Update store if they differ.
+   //doing it here is weird but it gets it done
+   if (oldApiSession.SessionId !== newAPISession.SessionId) {
+      store.dispatch(
+         updateAPISession({
+            apiSessionData: newAPISession,
+            apiSessionCreation: new Date().toISOString(),
+         })
+      );
+   }
+
+   return newAPISession;
 };
