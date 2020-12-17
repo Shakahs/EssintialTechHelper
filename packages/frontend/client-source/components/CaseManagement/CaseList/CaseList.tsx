af51395aca4e788289aff5c4ca7d25b82c32546b
Shakeel Saleemi <shakahs@gmail.com>
2020-12-17 11:05:25 -0800

    Fix duplicated tickets in ticket list.

--- packages/frontend/client-source/components/CaseManagement/CaseList/CaseList.tsx
+++ packages/frontend/client-source/components/CaseManagement/CaseList/CaseList.tsx
@@ -8,13 +8,27 @@ import {
    combiner,
 } from "../../../features/cases/caseSelectors";
 import Bool from "../../utility/Bool";
+import { caseStatusMapping } from "../../../constants";
 
 interface CaseListProps {}
 
 const CaseList: React.FunctionComponent<CaseListProps> = (props) => {
-   const arrivedCases = useSelector(getCasesArrive);
-   const enrouteCases = useSelector(getCasesEnroute);
-   const filteredCaseSummaries = useSelector(combiner);
+   const combinedCases = useSelector(combiner);
+
+   const arrivedCases = combinedCases.filter(
+      (c) => caseStatusMapping.Arrive.whenReading === c.UserStatus
+   );
+   const enrouteCases = combinedCases.filter(
+      (c) => caseStatusMapping.Enroute.whenReading === c.UserStatus
+   );
+
+   const otherCases = combinedCases.filter(
+      (c) =>
+         ![
+            caseStatusMapping.Enroute.whenReading,
+            caseStatusMapping.Arrive.whenReading,
+         ].includes(c.UserStatus)
+   );
 
    return (
       <div>
@@ -33,7 +47,7 @@ const CaseList: React.FunctionComponent<CaseListProps> = (props) => {
 
          <div className={"border-2 border-solid border-gray-500 p-2 mt-2"}>
             <p className={"text-2xl m-2"}>Ticket Queue:</p>
-            {map(filteredCaseSummaries, (sc) => (
+            {map(otherCases, (sc) => (
                <CaseListItem subcase={sc} key={sc.Id} />
             ))}
          </div>
