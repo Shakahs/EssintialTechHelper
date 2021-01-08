16109b0f4ffcb3ea58edae9a0c35925bd7eeebbe
Shakeel Saleemi <shakahs@gmail.com>
2021-01-08 13:55:15 -0800

    Hack together slightly better ETA/SLA display for CaseDisplayMini view. Definitely need a refactor here in the way SLA data is parsed and displayed.

--- packages/frontend/client-source/components/CaseManagement/CaseDisplay/CaseDisplayMini.tsx
+++ packages/frontend/client-source/components/CaseManagement/CaseDisplay/CaseDisplayMini.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
 import { CaseBase } from "../../../features/cases/types";
 import { partsList } from "../../../features/parts/partsList";
-import { findCaseStatusName } from "../../../features/cases/utility";
+import {
+   findCaseStatusName,
+   parseCaseSLA,
+} from "../../../features/cases/utility";
 import dateFormat from "date-fns/format";
 import parseISO from "date-fns/parseISO";
+import classnames from "classnames";
+import isToday from "date-fns/isToday";
+import { standardDateTimeFormatting } from "../../../constants";
 
 interface CaseDisplayMiniProps {
    subcase: CaseBase;
@@ -21,16 +27,33 @@ const CaseDisplayMini: React.FunctionComponent<CaseDisplayMiniProps> = (
    >
       <div>{props.subcase.Id}</div>
       <div>{`Priority: ${props.subcase.Priority}`}</div>
-      <div>{`${dateFormat(
+      <div>{`ETA ${dateFormat(
          parseISO(props.subcase.ScheduledDateTime),
          "L/d H:mm"
       )}`}</div>
       <div>{props.subcase.Location.City}</div>
       <div>{findCaseStatusName(props.subcase).name}</div>
-      <div>{`${dateFormat(
-         parseISO(props.subcase.ScheduledDateTime),
-         "L/d H:mm"
-      )}`}</div>
+      <div>
+         SLA:
+         {props.subcase.Milestones.map((ms) => {
+            //Big assumption here: the user is in the same time zone as the store.
+            //SLA is in the store's timezone, here we are reading it from the user
+            const parsedSLA_UTC = parseCaseSLA(ms.CalculatedDateTime);
+            return (
+               <span
+                  key={ms.Code}
+                  className={classnames("mr-2", {
+                     "bg-yellow-400": isToday(parsedSLA_UTC),
+                  })}
+               >
+                  {ms.Code === "ARR" && "Arrive"}
+                  {ms.Code === "FIX" && "Fix"}
+                  {dateFormat(parsedSLA_UTC, standardDateTimeFormatting)}
+                  {/*{ms.CalculatedDateTime}*/}
+               </span>
+            );
+         })}
+      </div>
       <div className={"col-span-3 lg:col-span-2"}>
          {props.subcase.CustomerCompany}
       </div>
